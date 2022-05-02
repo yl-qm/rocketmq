1533727bf3a9f8bdebd36eeb662245586c37fb97
Syl <379854097@qq.com>
2022-05-02 20:47:59 +0800

    v0.1 commit

--- rocketmq-consumer/src/main/java/com/syl/demo/serivce/ConsumerService.java
+++ rocketmq-consumer/src/main/java/com/syl/demo/serivce/ConsumerService.java
@@ -16,5 +16,6 @@ public class ConsumerService implements RocketMQListener<String> {
     @Override
     public void onMessage(String message) {
         System.out.println(message);
+        System.out.println("good");
     }
 }
